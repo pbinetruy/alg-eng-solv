e19769815aab352483d15206872a44bcdfdb5d75
Paul <paul.binetruy@centrale-marseille.fr>
2019-12-12 20:01:40 +0100

    [2nd debug]Cplex with memory usage limit and more precise

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -122,8 +122,8 @@ def vc_cplex():
     prob = cplex.Cplex()
     #To avoid printing the summary of the cplex resolution, to limit memory usage to 1.5GB and get more precise results on big graphs
     prob.set_results_stream(None)
-    parameters.workmem(1536)
-    parameters.mip.tolerances.mipgap(1e-08)
+    parameters.workmem = 1536
+    prob.parameters.mip.tolerances.mipgap = 1e-08
     #fill the CPLEX problem with all correct parameters
     prob.objective.set_sense(prob.objective.sense.minimize)
     prob.variables.add(obj=my_obj, ub=my_ub, types=my_ctype, names=my_colnames)
